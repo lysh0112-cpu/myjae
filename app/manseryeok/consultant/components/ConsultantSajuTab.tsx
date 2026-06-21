e777e70deaa2b006d39e397af8c60f52ee5486f2
lysh0112-cpu <lysh0112@gmail.com>
2026-06-21 14:36:02 +0900

    Update ConsultantSajuTab.tsx

--- app/manseryeok/consultant/components/ConsultantSajuTab.tsx
+++ app/manseryeok/consultant/components/ConsultantSajuTab.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect } from 'react'
 import SajuBoard from './SajuBoard'
+import YongsinBoard from './YongsinBoard'  // ✅ 추가
 import ElementScore from './ElementScore'
 import UnsungBoard from './UnsungBoard'
 import SinsalBoard from './SinsalBoard'
@@ -112,6 +113,7 @@ export default function ConsultantSajuTab({
       {saju.length > 0 && !converting && (
         <>
           <SajuBoard saju={saju} dayStem={dayStem} />
+          <YongsinBoard saju={saju} dayStem={dayStem} />  {/* ✅ 추가 */}
           <ElementScore />
           <UnsungBoard dayStem={dayStem} saju={saju} />
           <SinsalBoard saju={saju} yeonjji={yeonjji} iljji={iljji} />
